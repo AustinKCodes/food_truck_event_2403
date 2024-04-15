fb2e36e2dec1ba4e1616129856d4f3df93a9be6d
Austin Kenny <austinkennywork@gmail.com>
2024-04-15 12:02:55 -0400

    added stocking methods and test

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -6,5 +6,12 @@ attr_reader :name, :inventory
         @inventory = {}
     end
 
-    
+    def check_stock(item)
+        @inventory[item] || 0
+    end
+
+    def stock(item, quantity)
+        @inventory[item] ||= 0
+        @inventory[item] += quantity
+    end
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -22,4 +22,23 @@ RSpec.describe FoodTruck do
             expect(food_truck1.inventory).to eq({})
         end
     end
+
+    describe "item stocking" do
+        it "checks the inventory for stock" do
+            food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+            item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+            item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+            expect(food_truck1.check_stock(item1)).to eq(0)
+        end
+        
+        it "can stock items" do
+            food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+            item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+            item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+            food_truck1.stock(item1, 30)
+
+            expect(food_truck1.check_stock(item1)).to eq(30)
+        end
+    end
 end
