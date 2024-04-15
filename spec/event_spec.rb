ee2c034de7a6e05bcfc110055bdcd0c619bc956e
Austin Kenny <austinkennywork@gmail.com>
2024-04-15 12:27:02 -0400

    added item tracking test pom time

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -45,4 +45,25 @@ RSpec.describe Event do
             expect(event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
         end
     end
+
+    describe "trucks selling specfic items" do
+        it "can return specific items on trucks"
+        event = Event.new("South Pearl Street Farmers Market")
+            food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+            food_truck2 = FoodTruck.new("Ba-Nom-a-Nom") 
+            food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+            event.add_food_truck(food_truck1)
+            event.add_food_truck(food_truck2)
+            event.add_food_truck(food_truck3)
+
+            food_truck1.stock(item1, 35)
+            food_truck1.stock(item2, 7)
+            food_truck2.stock(item4, 50)
+            food_truck2.stock(item3, 25)
+            food_truck3.stock(item1, 65)
+
+            expect(event.food_trucks_that_sell(item1)).to eq()
+            expect(event.food_trucks_that_sell(item4)).to eq()
+    end
 end
