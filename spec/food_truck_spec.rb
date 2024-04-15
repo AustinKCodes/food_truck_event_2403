2c7de98703524166684d58a5fb174b2cb08a2226
Austin Kenny <austinkennywork@gmail.com>
2024-04-15 11:42:42 -0400

    Added food truck spec and class

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,19 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+
+    describe "initialization" do
+        it "can initialize" do
+            food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+
+            expect(food_truck1).to be_an_instance_of(FoodTruck)
+        end
+
+        it "has a name" do
+            food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+
+            expect(food_truck1.name).to eq("Rocky Mountain Pies")
+        end
+    end
+end
