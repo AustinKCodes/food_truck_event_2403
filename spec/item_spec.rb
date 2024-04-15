447845576607a5fb9735c4de02afede13b942fbf
Austin Kenny <austinkennywork@gmail.com>
2024-04-15 11:46:52 -0400

    forgot 2 test for item spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -11,8 +11,20 @@ RSpec.describe Item do
 
         it "can initialize" do
             item1 = Item.new({name: "Peach Pie", price: "$3.75"})
-            
+
             expect(item1).to be_an_instance_of(Item)
+        end
+
+        it "has a name" do
+            item1 = Item.new({name: "Peach Pie", price: "$3.75"})
+
+            expect(item1.name).to eq("Peach Pie")
+        end
+
+        it "has a moral cost" do
+            item1 = Item.new({name: "Peach Pie", price: "$3.75"})
+
+            expect(item1.price).to eq(3.75)
         end
     end
 end
